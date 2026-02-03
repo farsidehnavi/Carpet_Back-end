644c26ca7b8a5dabde07956d89e7348a5832e582
Hesam <farsidehnavih@gmail.com>
2026-02-04 02:08:16 +0330

    1

--- src/connectDb.js
+++ src/connectDb.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "hesam", // your PostgreSQL user
-  host: "156.236.31.234", // your VPS IP or localhost
+  host: "156.236.31.135", // your VPS IP or localhost
   database: "carpet", // your database name
   password: "WebBuilding", // your DB password
   port: 5432, // PostgreSQL default port
